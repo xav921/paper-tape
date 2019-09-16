e647d34404916ad421c6ae4afca1494920e836dd
xav921 <xav7470@gmail.com>
2019-09-16 13:56:40 -0600

    Addition is now functional.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Paper Tape
+
+Adds up all of the numbers from the command line arguments and prints a total.
+
+Note: Currently supports integers only.
--- src/edu/cnm/deepdive/PaperTape.java
+++ src/edu/cnm/deepdive/PaperTape.java
@@ -3,9 +3,25 @@ package edu.cnm.deepdive;
 public class PaperTape {
 
   public static void main(String[] args) {
-
+    int[] values = parseInts(args);
+    long total = sum(values);
+    System.out.printf("Total = %,d%n", total);
   }
 
-  private static 
+  private static int[] parseInts(String[] args) {
+    int[] values = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      values[i] = Integer.parseInt(args[i]);
+    }
+    return values;
+  }
+
+  private static long sum(int[] data) {
+    long total = 0;
+    for (int val : data) {
+      total += val;
+    }
+    return total;
+  }
 
 }
